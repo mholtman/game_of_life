3f4851f014ed54f25f3b36c7b3f0264d7ff8384b
R.J. Osborne <rosborne@pillartechnology.com>
2017-11-18 20:47:17 -0500

    stepping a single live cell returns an empty world

--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -4,4 +4,9 @@ defmodule GameOfLifeTest do
   test "an empty world returns an empty world" do
     assert MapSet.new == GameOfLife.step(MapSet.new)
   end
+
+  test "a world with a single live cell returns an empty world" do
+    world = [{0,0}] |> MapSet.new
+    assert MapSet.new == GameOfLife.step(world)
+  end
 end
